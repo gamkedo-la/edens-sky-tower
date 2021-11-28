7172f11f01ba292500fcaf369293dc8d3079af7b
Simon <sj.hoffiz@gmail.com>
2021-11-27 23:35:41 -0500

    Ring Levels Trigger Separately

---
+++ Assets/Scripts/Ring01.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ring01 : MonoBehaviour
+{
+    public Transform target;
+    public float t;
+    bool triggerRing = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Script started for: " + gameObject.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            //Debug.Log("Space key was pressed.");
+            triggerRing = true;
+        }
+        
+        if(triggerRing) {
+            Vector3 a = transform.position;
+            Vector3 b = target.position;
+            transform.position = Vector3.Lerp(a,b, t); 
+        }
+        
+    }
+}
