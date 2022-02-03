11dbae33ac6f62ea9730653648a4b57c330fd82e
Simon <sj.hoffiz@gmail.com>
2022-02-03 16:25:28 -0500

    cleaned up old key scripts

--- Assets/Scripts/ShardPosition.cs
+++ Assets/Scripts/ShardPosition.cs
@@ -7,10 +7,11 @@ public class ShardPosition : MonoBehaviour
     public string keyNumber;
     private bool shardActivated = false;
     public Transform ShardEngagementDestination;
+    private Vector3 startLocalPosition;
 
     void Start()
     {
-
+        startLocalPosition = transform.localPosition;
     }
 
     void Update()
@@ -19,7 +20,9 @@ public class ShardPosition : MonoBehaviour
 
         if(shardActivated) {
             gameObject.transform.position = ShardEngagementDestination.position;
-        }      
+        } else {
+            gameObject.transform.localPosition = startLocalPosition;
+        }  
     }
 
 }
