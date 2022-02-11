59742b1c8e561fac678c84aa54555acba3517caa
Simon <sj.hoffiz@gmail.com>
2022-02-11 12:28:14 -0500

    improved code for rotating shards/objects

---
+++ Assets/Scripts/HorizontalObjectRotation.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalObjectRotation : MonoBehaviour
+{
+    public GameObject controlledObject;
+    bool controllingObject = false;
+    Player playerScript;
+
+    
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(controllingObject) {
+            if (Input.GetKey (KeyCode.A)) {
+                controlledObject.transform.Rotate(0, 01, 0, Space.Self);
+            }
+
+            if (Input.GetKey (KeyCode.D)) {
+                controlledObject.transform.Rotate(0, -01, 0, Space.Self);
+            }
+
+            if (Input.GetKeyDown (KeyCode.S)) {
+                controllingObject = false;
+                playerScript.enabled = true;
+            }
+        }
+    }
+
+    void OnCollisionEnter (Collision collision) {
+        //checking collision to control objects
+
+        if(collision.gameObject.CompareTag ("Player")) {
+            playerScript = collision.gameObject.GetComponent<Player>();
+            playerScript.enabled = false;
+            controllingObject = true;
+         }
+    }
+}
