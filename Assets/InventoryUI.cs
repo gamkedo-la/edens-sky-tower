ded1d591e4d134b2ef9a9161f3553ae56dde8007
wmckay555 <67658851+wmckay555@users.noreply.github.com>
2022-02-11 17:03:37 -0800

    Added small animation to opening and closing the inventory

--- Assets/InventoryUI.cs
+++ Assets/InventoryUI.cs
@@ -7,17 +7,35 @@ public class InventoryUI : MonoBehaviour
 {
     public Text[] TextBoxes;
 
+    bool open = false;
+
+    public float OpenPos;
+    public float ClosePos;
+
+    public float AnimSpeed = 1;
+
+    private float A;
+    private float B;
+    private float T = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        A = ClosePos;
+        B = ClosePos;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GetComponent<RectTransform>().anchoredPosition = new Vector2(Mathf.Lerp(A, B, T), 30);
+        T += Time.deltaTime * AnimSpeed;
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            OpenClose();
+        }
+
     }
 
     public void UpdateUI(int[] numbers)
@@ -26,8 +44,24 @@ public class InventoryUI : MonoBehaviour
         {
             TextBoxes[i].text = numbers[i].ToString();
         }
-
-
-
+               
     }
+
+    public void OpenClose()
+    {
+
+        if(open == true)
+        {
+            A = OpenPos;
+            B = ClosePos;
+        }
+        else
+        {
+            A = ClosePos;
+            B = OpenPos;
+        }
+        T = 0;
+        open = !open;
+    }
+
 }
