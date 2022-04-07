f1ed08994b3650203a1e7bf576e628d2ec9c8963
Armando Navarrete <viking805fan@yahoo.com>
2022-04-06 21:07:06 -0700

    W.I.P. Laser & Mirror Player Input Rotation.

---
+++ Assets/Scripts/Level3/LaserMirrorMovement.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LaserMirrorMovement : MonoBehaviour
+{
+    private bool triggering;
+
+    public Text toolTipText;
+
+    public Vector3 point; //position of the point you want to rotate around
+
+    void Start()
+    {
+        toolTipText.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(triggering)
+        {
+            if (Input.GetKey(KeyCode.U))
+            {
+                print("Console has been activated.");
+                transform.RotateAround(point, Vector3.up, 20 * Time.deltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.I))
+            {
+                print("Console has been activated.");
+                transform.RotateAround(point, -Vector3.up, 20 * Time.deltaTime);
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            toolTipText.text = "Rotate Left = U\n";
+            toolTipText.text += "Rotate Right = I\n";
+
+            toolTipText.gameObject.SetActive(true);
+            triggering = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            toolTipText.gameObject.SetActive(false);
+            triggering = false;
+        }
+    }
+
+    
+}
