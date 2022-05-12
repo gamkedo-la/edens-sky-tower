d46a66f43c219702e1cfa94e3163f67d722cf67a
Simon <sj.hoffiz@gmail.com>
2022-05-12 09:51:20 -0400

    WIP - SoundManager.cs for playing story narration files

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public static AudioClip storyNarrationOne, storyNarrationTwo;
+    static AudioSource audioSrc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        storyNarrationOne = SFX.Load<AudioClip>("SFX_Ambience_Space_Crickets");
+        storyNarrationTwo = SFX.Load<AudioClip>("SFX_Ambience_Space_Wind");
+
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void PlaySound (string clip)
+    {
+        switch (clip)
+        {
+            case "narrationOne":
+                audioSrc.PlayOneShot(storyNarrationOne);
+                    break;
+            case "narrationTwo":
+                audioSrc.PlayOneShot(storyNarrationTwo);
+                    break;
+        }
+    }
+}
