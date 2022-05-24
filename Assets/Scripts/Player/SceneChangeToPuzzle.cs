c738b1033b3697cb07009f853d098310794e3dae
Gamkedo <chris@gamkedo.com>
2022-05-23 20:47:27 -0400

    Obelisk prefabs show different form if they've been used

--- Assets/Scripts/Player/SceneChangeToPuzzle.cs
+++ Assets/Scripts/Player/SceneChangeToPuzzle.cs
@@ -7,8 +7,20 @@ public class SceneChangeToPuzzle : MonoBehaviour
 {
     public string sceneToLoad;
     public static int destroyCollider = 0;
-    public InventoryUI sotryTabletUICount;
-    public float puzzleSceneCounter = 0; // deferentiator to checking that its puzzle scene number 4
+
+    public GameObject unsolvedForm;
+    public GameObject solvedForm;
+
+    private void Start() {
+        if (unsolvedForm == null || solvedForm == null) {
+            Debug.Log(gameObject.name + " is missing its solved/unsolved form, replace with prefab");
+            return;
+        }
+        // NOTE: RELIES ON THE PARENT (P_Obelisk) being a unique name per each one
+        bool isSolved = PlayerPrefs.GetInt(PlayerPrefsObeliskKey(), 0) == 1;
+        unsolvedForm.SetActive(isSolved == false);
+        solvedForm.SetActive(isSolved);
+    }
 
     void Update ()
     {
@@ -19,20 +31,27 @@ public class SceneChangeToPuzzle : MonoBehaviour
     }
 
     void OnTriggerEnter (Collider other) {
-
-        if(puzzleSceneCounter != 0)
-        {
-            if(sotryTabletUICount.StoryCount == 6)
-            {
-                Debug.Log("scene 4 loaded - all story bits");
-                SceneManager.LoadScene(sceneToLoad);
+        int storyCount = 0;
+        for(int i=1;i<=7;i++) {
+            if (PlayerPrefs.GetInt("seenTablet"+i, 0) == 1) {
+                storyCount++;
             }
+        }
+        if (storyCount >= 6)
+        {
+            Debug.Log("scene 4 loaded - all story bits"); 
+            SceneManager.LoadScene(sceneToLoad);
         } else
         {
             //destroyCollider = destroyCollider + 1;
             Debug.Log("normal scene load");
+            PlayerPrefs.SetInt(PlayerPrefsObeliskKey(), 1);
             SceneManager.LoadScene(sceneToLoad);
         }        
+    }
+
+    string PlayerPrefsObeliskKey() {
+        return "ObeliskStateOf" + transform.parent.name;
     }
 
 }
