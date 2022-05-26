6aa46bc4edf29d5973c833150a20b762e4ff92c3
Simon <sj.hoffiz@gmail.com>
2022-05-26 18:04:25 -0400

    elevator platform resets position when player enters tower shard

--- Assets/Scripts/shardCloseBridgeAndDoor.cs
+++ Assets/Scripts/shardCloseBridgeAndDoor.cs
@@ -6,11 +6,13 @@ public class shardCloseBridgeAndDoor : MonoBehaviour
 {
     public towerBridgeMove towerBridgeMove;
     public towerDoorMove towerDoorMove;
+    public ElevatorPlatform elevPlatformScript;
 
 
     void OnTriggerEnter (Collider other) {
         towerBridgeMove.CloseTowerBridge();
         towerDoorMove.CloseTowerDoor();
+        elevPlatformScript.resetElevPlatform = true;
 
     }
 }
